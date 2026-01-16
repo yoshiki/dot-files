318afaf0aa84b98817b8b678c315c48d2d98ab6a
Yoshiki Kurihara <clouder@gmail.com>
2026-01-17 01:22:32 +0900

    Add Ghostty config and update install.sh path variables

--- install.sh
+++ install.sh
@@ -3,32 +3,34 @@
 echo "Now copying file..."
 
 PWD=`pwd`
-ln -sf $PWD/zshrc/.zshrc ~/
+ln -sf $PWD/zshrc/.zshrc $HOME/
 
-#ln -sf $PWD/tcshrc/.tcshrc ~/
-#ln -sf $PWD/tcshrc/.tcshrc.alias ~/
-#ln -sf $PWD/tcshrc/.tcshrc.complete ~/
-#ln -sf $PWD/tcshrc/.tcshrc.set ~/
-#ln -sf $PWD/tcshrc/.tcshrc.bindkey ~/
+#ln -sf $PWD/tcshrc/.tcshrc $HOME/
+#ln -sf $PWD/tcshrc/.tcshrc.alias $HOME/
+#ln -sf $PWD/tcshrc/.tcshrc.complete $HOME/
+#ln -sf $PWD/tcshrc/.tcshrc.set $HOME/
+#ln -sf $PWD/tcshrc/.tcshrc.bindkey $HOME/
 
-#ln -sf $PWD/cvsrc/.cvsrc ~/
+#ln -sf $PWD/cvsrc/.cvsrc $HOME/
 
-ln -sf $PWD/emacs/.emacs ~/
-ln -sf $PWD/emacs/.emacs.d ~/
+ln -sf $PWD/emacs/.emacs $HOME/
+ln -sf $PWD/emacs/.emacs.d $HOME/
 
-#ln -sf $PWD/screenrc/.screenrc ~/
+#ln -sf $PWD/screenrc/.screenrc $HOME/
 
-ln -sf $PWD/skk/.skk ~/
+ln -sf $PWD/skk/.skk $HOME/
 
-ln -sf $PWD/termcap/.termcap ~/.termcap
+ln -sf $PWD/termcap/.termcap $HOME/.termcap
 
-#ln -sf $PWD/keysnail/.keysnail.js ~/.keysnail.js
+#ln -sf $PWD/keysnail/.keysnail.js $HOME/.keysnail.js
 
-ln -sf $PWD/tmux/.tmux.conf ~/.tmux.conf
+ln -sf $PWD/tmux/.tmux.conf $HOME/.tmux.conf
 
-ln -sf $PWD/vimrc/.vimrc ~/.vimrc
+ln -sf $PWD/vimrc/.vimrc $HOME/.vimrc
 
-tic $PWD/terminfo/xterm-color.ti
+ln -sf $PWD/ghostty/config $HOME/.config/ghostty/config
+
+#tic $PWD/terminfo/xterm-color.ti
 
 echo done
 
